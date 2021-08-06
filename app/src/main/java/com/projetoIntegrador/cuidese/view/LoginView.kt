50d1d6d239c4ffbed333f99c45b8c7c9ce5d5e50
jessica-lemes <jessica.costa.moraes@gmail.com>
2021-08-05 21:36:13 -0300

    First Commit

---
+++ app/src/main/java/com/projetoIntegrador/cuidese/view/LoginView.kt
@@ -0,0 +1,16 @@
+package com.projetoIntegrador.cuidese.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.projetoIntegrador.cuidese.R
+
+class LoginView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+
+
+
+}
